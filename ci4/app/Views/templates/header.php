7ad638c28955fb48a503b94932df30abe41147c5
esarong <esarong@student.apc.edu.ph>
2023-02-17 09:36:57 +0800

    headers

--- ci4/app/Views/templates/header.php
+++ ci4/app/Views/templates/header.php
@@ -1,9 +1,11 @@
 <!doctype html>
 <html>
 <head>
-<?php echo link_tag('css/mystyle.css'); ?>
+<?php echo link_tag('css/styles.css'); ?>
     <title>CodeIgniter Tutorial</title>
 </head>
 <body>
-
-    <h1><?= esc($title) ?></h1>
+<?php
+// <h1><?= esc($title) </h1>
+?>
+</body>
