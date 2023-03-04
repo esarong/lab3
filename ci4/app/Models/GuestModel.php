777c7878cb68a7394ae687c5661f099316af0811
esarong <119904012+esarong@users.noreply.github.com>
2023-03-04 10:03:02 +0800

    guestmodel updated

--- ci4/app/Models/GuestModel.php
+++ ci4/app/Models/GuestModel.php
@@ -6,7 +6,7 @@ use CodeIgniter\Model;
 
 class GuestModel extends Model
 {
-    protected $table = 'esarong_myguest';
+    protected $table = 'esarong_myguests';
 
     protected $allowedFields = ['name', 'email', 'website', 'comment', 'gender'];
 
